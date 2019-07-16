0c7c51c4ed9c700f2677fedd98acc1e5da85d4b5
Raul Sposito <raulsposito@gmail.com>
2019-07-16 13:22:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,10 @@
 # Write your code here.
 def badge_maker(name)
+  return "Hello, my name is #{name}."
+end 
+
+
+def badge_maker(name)
   name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus"]
   puts "Hello, my name is #{name}."
 end
