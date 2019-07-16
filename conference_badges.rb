c7e49a2e0b148ff676f0eccf978cb141472c242a
Raul Sposito <raulsposito@gmail.com>
2019-07-16 13:25:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,5 +2,13 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end 
-name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus"]
 
+names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus"]
+
+def batch_badge_creator(names)
+  badges=[]
+  names.each do|badge|
+  badges.push("Hello, my name is #{badge}.")
+  end
+  return badges
+end
